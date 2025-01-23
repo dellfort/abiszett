e2f575bd07b6c59fb812699f32450650f6b742c3
Felix Toth <felix.toth.extern@bdr.de>
2025-01-23 16:08:38 +0100

    now it is fixed

---
+++ public/index.html.d.ts
@@ -0,0 +1,1 @@
+export declare function renderHTML(): Promise<string>;
---
+++ public/index.html.js
@@ -0,0 +1,22 @@
+import { html, render } from "@lit-labs/ssr";
+import { collectResultSync } from "@lit-labs/ssr/lib/render-result.js";
+export async function renderHTML() {
+    const template = html `
+    <!DOCTYPE html>
+    <html lang="de">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="csp-nonce" name="csp-nonce" property="csp-nonce" content="__NONCE_VALUE__">
+        
+     
+        <title>opening hours</title>
+      </head>
+      <body>
+            Opening hours app
+      </body>
+    </html>
+  `;
+    const result = render(template);
+    return collectResultSync(result);
+}
